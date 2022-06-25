4410e45861b17940784894d0d14f85983bdc27bd
passmanagerapp <passmanagerapplication@gmail.com>
2022-06-25 13:39:54 +0300

    push existing code

---
+++ app/src/main/java/com/akilincarslan/compose/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.akilincarslan.compose
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+
+}
+
