898671131e0d1c0e6e2aa658d076b83b2349e803
passmanagerapp <passmanagerapplication@gmail.com>
2022-06-25 21:24:07 +0300

    rows and columns

--- app/src/main/java/com/akilincarslan/compose/MainActivity.kt
+++ app/src/main/java/com/akilincarslan/compose/MainActivity.kt
@@ -4,22 +4,73 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.akilincarslan.compose.ui.theme.Typography
 import kotlinx.coroutines.launch
+import org.intellij.lang.annotations.JdkConstants
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            RowBasics()
         }
     }
 
+    @Composable
+    private fun ColumnBasics() {
+        Column(
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Blue)
+                .padding(vertical = 15.dp)
+        ) {
+            Text(
+                text = "Hello!",
+                color = Color.Green,
+                style = Typography.h5,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Black)
+            )
+            Text(
+                text = "Columns",
+                color = Color.Red,
+                style = Typography.h6
+            )
+        }
+    }
+
+    @Composable
+    fun RowBasics() {
+        Row(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            ColumnBasics()
+        }
+    }
 }
 
